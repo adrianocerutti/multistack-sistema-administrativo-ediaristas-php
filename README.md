a560337ccc94e532c493be374dec61523046c345
Adriano B. Cerutti <adrianocerutti@gmail.com>
2021-08-13 11:57:16 -0300

    Atualização do README.md, criação das Views de usuário, criaçã do usuário Admin via Seed e atulizações relacionadas a ele

--- README.md
+++ README.md
@@ -6,15 +6,21 @@ Desenvolvido no curso multi-stack da treinaweb
 
 #### Clonar o repositório
 
+```
 git clone https://github.com/adrianocerutti/multistack-sistema-administrativo-ediaristas-php.git
+```
 
 #### Instalar as dependências
 
+```
 composer install
+```
 
 Ou em ambiente de desenvolvimento:
 
+```
 composer update
+```
 
 #### Criar arquivo de configurações de ambiente
 
@@ -23,8 +29,21 @@ configurar os detalhes da aplicação e conexão com o banco de dados
 
 ### Criar a estrutura no banco de dados
 
+```
 php artisan migrate
+```
+
+#### Criar o usuário admin
+
+```
+php artisan db:seed
+```
+
+Usuario criado: admin@admin.com
+Senha: 123123123
 
 #### Iniciar o servidor de desenvolvimento
 
+```
 php artisan serve
+```
--- config/adminlte.php
+++ config/adminlte.php
@@ -225,45 +225,7 @@ return [
     */
 
     'menu' => [
-        // Navbar items:
-        /* [
-            'type'         => 'navbar-search',
-            'text'         => 'search',
-            'topnav_right' => true,
-        ], 
-        [
-            'type'         => 'fullscreen-widget',
-            'topnav_right' => true,
-        ],*/
-
         // Sidebar items:
-        /* [
-            'type' => 'sidebar-menu-search',
-            'text' => 'search',
-        ],
-        [
-            'text' => 'blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
-        ],
-        [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
-        ],
-        ['header' => 'account_settings'],
-        [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
-        ],
-        [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
-        ], */
         [
             'text'    => 'Cadastro de Serviços',
             'icon'    => 'fas fa-fw fa-broom',
@@ -278,48 +240,24 @@ return [
                     'route'  => 'servicos.create',
                     'icon'    => 'fas fa-fw fa-file',
                 ],
-                /* [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ], */
             ],
         ],
-        /* ['header' => 'labels'],
         [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
+            'text'    => 'Cadastro de Usuários',
+            'icon'    => 'fas fa-fw fa-user',
+            'submenu' => [
+                [
+                    'text' => 'Lista de Usuários',
+                    'route'  => 'usuarios.index',
+                    'icon'    => 'fas fa-fw fa-list',
+                ],
+                [
+                    'text' => 'Novo Usuário',
+                    'route'  => 'usuarios.create',
+                    'icon'    => 'fas fa-fw fa-file',
+                ],
+            ],
         ],
-        [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
-        ], */
     ],
 
     /*
